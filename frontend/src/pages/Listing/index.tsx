e9037418933fc43b57ebd803707435e6ba38586b
EricBerlim <ericberlim@gmail.com>
2022-01-16 08:41:18 -0300

    Netlify

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -6,8 +6,8 @@ function Listing() {
     return (
         <>
             <Pagination />
-
-            <div className="conrainer">
+            
+            <div className="container">
                 <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
